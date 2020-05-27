2bd988a58fb2e9c06e76e626659b07671492cfca
Freezman13 <58755601+Freezman13@users.noreply.github.com>
2020-05-27 18:26:18 -0500

    Asteroids spawn at a random border
    Added simple logic to spawn the asteroids at one of the primary borders.

--- Source/Asteroids_Clone/Asteroid_Spawner.cpp
+++ Source/Asteroids_Clone/Asteroid_Spawner.cpp
@@ -22,21 +22,56 @@ UAsteroid_Spawner::UAsteroid_Spawner()
 void UAsteroid_Spawner::BeginPlay()
 {
 	Super::BeginPlay();
-
-	//if (!IsValid(Spawned_Asteroids))
-	//{
-
-		int Number_Of_Asteroids = 5;
-		for (int32 Asteroid_Number = 0; Asteroid_Number < Number_Of_Asteroids; Asteroid_Number++)
+	
+	
+	
+	
+	int Number_Of_Asteroids = 5;
+	for (int32 Asteroid_Increment = 0; Asteroid_Increment < Number_Of_Asteroids; Asteroid_Increment++)
+	{
+		// selects near which border the asteroid will spawn
+		int Border_Spawn_Selection = FMath::RandRange(0, 3); // clockwise starting top
+		
+		// top
+		if (Border_Spawn_Selection == 0) 
 		{
-			int Random_Location_X = FMath::RandRange(0, 1);
-			int Random_Location_Y = FMath::RandRange(0, 900);
+			int Random_Location_X = 1300.0;
+			int Random_Location_Y = FMath::RandRange(-2000, 2000);
 			FVector Location = FVector(Random_Location_X, Random_Location_Y, 290.0);
 			FRotator Rotation = FRotator(0, 0, 0);
 			Spawn_Asteroid(Location, Rotation);
 		}
-	//}
-		
+
+		// right
+		else if (Border_Spawn_Selection == 1)
+		{
+			int Random_Location_X = FMath::RandRange(-1300, 1300);
+			int Random_Location_Y = 2300.0;
+			FVector Location = FVector(Random_Location_X, Random_Location_Y, 290.0);
+			FRotator Rotation = FRotator(0, 0, 0);
+			Spawn_Asteroid(Location, Rotation);
+		}
+
+		// bottom
+		else if (Border_Spawn_Selection == 2)
+		{
+			int Random_Location_X = -1300.0;
+			int Random_Location_Y = FMath::RandRange(-2000, 2000);
+			FVector Location = FVector(Random_Location_X, Random_Location_Y, 290.0);
+			FRotator Rotation = FRotator(0, 0, 0);
+			Spawn_Asteroid(Location, Rotation);
+		}
+
+		// left
+		else if (Border_Spawn_Selection == 3)
+		{
+			int Random_Location_X = FMath::RandRange(-1300, 1300);
+			int Random_Location_Y = -2300.0;
+			FVector Location = FVector(Random_Location_X, Random_Location_Y, 290.0);
+			FRotator Rotation = FRotator(0, 0, 0);
+			Spawn_Asteroid(Location, Rotation);
+		}
+	}
 }
 
 
@@ -44,8 +79,15 @@ void UAsteroid_Spawner::BeginPlay()
 void UAsteroid_Spawner::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
 	
+	// somehow check that some number of asteroid instances exist and spawn more if needed
+	// or somehow pass a check from asteroid_actor after any asteroid is destroyed
+	/*
+	if (!IsValid(Spawned_Asteroids)) 
+	{
+		// logic from begin play goes here
+	}
+	*/
 }
 	
 
